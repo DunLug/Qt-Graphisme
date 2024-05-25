a30ea1e6af031261a41c99bea63f0ff7d513a8fc
Duncan Luguern <duncan.luguern@u-bourgogne.fr>
2024-05-25 13:15:17 +0200

    Documentation de Background

--- background.h
+++ background.h
@@ -4,6 +4,8 @@
 #include "drawable.h"
 #include <QPaintEvent>
 #include <QDebug>
+
+/// Classe utilisée dans Fenetre comme widget central, celui-ci permet d'afficher une image de fond et de gérer le redimensionnement de la fenêtre en avertissant les widgets enfants
 class Background : public Drawable
 {
     Q_OBJECT
@@ -19,9 +21,11 @@ public:
     void paintEvent(QPaintEvent *event){qDebug() << event->rect();};
 
 private:
+    /// La taille de base du Background
     const QSize base_size = QSize(1280, 600);
 
 signals:
+    /// Signal émis en cas de redimensionnement de la fenêtre. Celui indique des facteurs de redimensionnement utilisés par les widgets
     void resized(double, double);
 };
 
